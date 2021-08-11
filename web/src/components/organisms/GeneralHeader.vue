aa69942e190609e9485224857948935e3e1cc129
terrafkube <terraf.kube@gmail.com>
2021-08-11 11:03:24 +0900

    プロジェクトのセットアップ

---
+++ web/src/components/organisms/GeneralHeader.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<!-- アイコンを置くエリア -->
+		<!-- 各画面への遷移メニュー -->
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ web/src/components/templates/CampResultInputTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+		<!-- header -->
+    <header></header>
+		<!-- sidemenu -->
+    <nav></nav>
+		<!-- content -->
+    <main></main>
+		<!-- footer -->
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
